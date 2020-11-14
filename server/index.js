990398dc07da8fef7192d86be00e8dae030be2fe
Nicodemus Opon <nicodemusopon@gmail.com>
2020-11-14 19:29:07 +0300

    2020-11-14T19:29

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const store = require('data-store')({ path: process.cwd() + 'store.rootless' });
+const store = require('data-store')({ path: process.cwd() + '/store/.store.json' });
 const path = require('path');
+const cors = require('cors');
 
-require('custom-env').env()
+require('custom-env').env("staging")
 var crypto = require('crypto');
 // Difining algorithm 
 const algorithm = process.env.ENCRYPTION_ALGORITHM;
@@ -13,8 +14,11 @@ var authKey = process.env.APIKEY;
 const app = express()
 const router = express.Router();
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/', router);
+app.use(express.static("public"));
 
 const port = process.env.PORT
 
@@ -56,12 +60,59 @@ function filterBy(list, criteria) {
 function url(urln) {
     return (path.join(__dirname + '/html/' + urln))
 }
+function isContainedIn(a, b) {
+    if (typeof a != typeof b)
+        return false;
+    if (Array.isArray(a) && Array.isArray(b)) {
+        // assuming same order at least
+        for (var i = 0, j = 0, la = a.length, lb = b.length; i < la && j < lb; j++)
+            if (isContainedIn(a[i], b[j]))
+                i++;
+        return i == la;
+    } else if (Object(a) === a) {
+        for (var p in a)
+            if (!(p in b && isContainedIn(a[p], b[p])))
+                return false;
+        return true;
+    } else
+        return a === b;
+}
+function oupdate(obj/*, …*/) {
+    for (var i = 1; i < arguments.length; i++) {
+        for (var prop in arguments[i]) {
+            var val = arguments[i][prop];
+            if (typeof val == "object") // this also applies to arrays or null!
+                update(obj[prop], val);
+            else
+                obj[prop] = val;
+        }
+    }
+    return obj;
+}
+function log(x) {
+    console.clear();
+    console.log(x);
+}
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/html/index.html'));
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 })
 app.get('/api', (req, res) => {
     res.send(message("success", "rootless server running", "200", "SERVER_OK"));
+})
+app.get('/dump', (req, res) => {
+    if (auth(req.query.auth)) {
+        res.send(store.data);
+    } else {
+        res.send(message("error", "Unable to authenticate,invalid or missing apikey", "401", "UNAUTHORIZED"));
+    }
+})
+app.get('/model', (req, res) => {
+    if (auth(req.query.auth)) {
+        res.send(Object.keys(store.data));
+    } else {
+        res.send(message("error", "Unable to authenticate,invalid or missing apikey", "401", "UNAUTHORIZED"));
+    }
 })
 //i.e /api/user
 app.route('/api/:model')
@@ -70,7 +121,6 @@ app.route('/api/:model')
         var model = req.params.model;
         var fquery = req.query;
         delete fquery.auth;
-        console.log(fquery)
         if (auth(mkey)) {
             if (Object.keys(fquery).length > 0) {
                 res.send(filterBy(store.data[model], fquery))
@@ -82,9 +132,6 @@ app.route('/api/:model')
                     res.send(response)
                 }
             }
-
-
-
         } else {
             res.send(message("error", "Unable to authenticate,invalid or missing apikey", "401", "UNAUTHORIZED"));
         }
@@ -104,26 +151,55 @@ app.route('/api/:model')
      */
     .delete(function (req, res) {
         var model = req.params.model;
-        var body = req.body;
-        var conct = body.target.split(".")
-        todelete = conct[0];
-        tar = conct[1];
-
-        items = store.data[model];
-        var i = items.length;
-        for (let i = 0; i < items.length; i++) {
-            var element = items[i];
-            console.log(element);
-            if (items[i][todelete] === tar) {
+        //var body = req.body;
+        var affected = 0
+        console.log(store.data[model].length)
+        for (var i = store.data[model].length - 1; i > -1; i--) {
+            if (isContainedIn(req.body.query, store.data[model][i])) {
                 store.data[model].splice(i, 1);
+                //delete store.data[model][i]
+                affected++
             }
         }
         store.save();
-        res.send(store.get(model))
+        var msg = affected + " row(s) affected";
+        res.send(message("success", msg, "200", "OK"))
+        /* var model = req.params.model;
+         var body = req.body;
+         var conct = body.target.split(".")
+         todelete = conct[0];
+         tar = conct[1];
+ 
+         items = store.data[model];
+         var i = items.length;
+         for (let i = 0; i < items.length; i++) {
+             var element = items[i];
+             console.log(element);
+             if (items[i][todelete] === tar) {
+                 store.data[model].splice(i, 1);
+             }
+         }
+         store.save();
+         res.send(store.get(model))*/
     })
-app.use('/', router);
-app.use(express.static("public"));
+    .put(function (req, res) {
+        var model = req.params.model;
+        //var body = req.body;
+        var affected = 0
+        for (let i = 0; i < store.data[model].length; i++) {
+            if (isContainedIn(req.body.query, store.data[model][i])) {
+                store.data[model][i] = oupdate(store.data[model][i], req.body.data);
+                affected++
+
+            }
+        }
+        store.save()
+        console.log(affected);
+        //var vv = filterBy(store.data[model][0], req.body.query)
+        var msg = affected + " row(s) affected";
+        res.send(message("success", msg, "200", "OK"))
+    })
 
 app.listen(port, () => {
-    console.log(`Rootless listening at http://localhost:${port}`)
+    console.log(`Rootless 🌲 listening at http://localhost:${port}`)
 })
